d57df05e70fc63d49b46247b18e1f45ff7253a53
RyanT-git <31804248+RyanT-git@users.noreply.github.com>
2024-03-09 14:31:12 -0500

    Resume Website
    Finished Resume Website

--- Resume/R-T-Resume-website-V1.py
+++ Resume/R-T-Resume-website-V1.py
@@ -1,24 +1,13 @@
 from flask import *
 app = Flask(__name__)
 
-myInfo = """
-<!DOCTYPE html>
-<html>
-<body>
-<h1>About Me</h1>
-<h2> [info] </h2>
-</body>
-</html>
-"""
-
-
 @app.route('/')
 def you_found_me():
    return render_template('resume.html')
-   
-@app.route('/about')
-def upload():
-	return myInfo
+
+@app.route('/codepath')
+def pdf_viewer():
+	return redirect("./static/Course Overview - CYB102 _ Intermediate Cybersecurity _ CodePath Courses.pdf")
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port = 4242, debug = True)
